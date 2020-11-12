d73df2b0cbeeae136904bb6017d56b9c9b1077d9
Pfzelda95 <68494587+Pfzelda95@users.noreply.github.com>
2020-11-12 23:50:28 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master 3/sketch.js
@@ -0,0 +1,79 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var base,testparticle,testplinko;
+
+
+var score = 0;
+var particles = [];
+var plinkos = [];
+var divs = [];
+
+var divheight = 300;
+
+function setup() {
+  createCanvas(485,800);
+  engine = Engine.create();
+    world = engine.world;
+    for (var k = 0;k <=width;k = k + 80){
+      divs.push(new div(k,height-divheight/2,10,divheight));
+    //  divs[k].display();
+    }
+    for(var j = 40;j <=width; j = j+50){
+      plinkos.push(new plinko(j,75,10))
+    }
+    for(var j = 15;j <=width-10; j = j+50){
+      plinkos.push(new plinko(j,175,10))
+    }
+    for(var j = 40;j <=width; j = j+50){
+      plinkos.push(new plinko(j,275,10))
+    }
+    for(var j = 40;j <=width; j = j+50){
+      plinkos.push(new plinko(j,375,10))
+    }
+
+    if(frameCount%60===0){ 
+    particles.push(new particle(random(width/2-30, width/2+30), 10,10));
+  }
+  base = new ground(240,800,500,15)
+  
+}
+
+
+
+function draw() {
+  background("black"); 
+  Engine.update(engine);
+   
+  
+  for (var k = 0; k < divs.length; k++) {
+    divs[k].display();
+  }
+  for (var j = 0; j < plinkos.length; j++) {
+    plinkos[j].display();
+  }
+  for (var j = 0; j < plinkos.length; j++) {
+    plinkos[j].display();
+  }
+  
+  for (var j = 0; j < plinkos.length; j++) {
+    plinkos[j].display();
+  }
+  for (var j = 0; j < plinkos.length; j++) {
+    plinkos[j].display();
+  }
+  for (var l = 0; l < plinkos.length; l++) {
+    particles[l].display();
+  }
+  
+  
+
+  
+  console.log(particles)
+  
+  base.display();
+  
+  
+  
+}
